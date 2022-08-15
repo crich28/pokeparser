0b7bee8b9ac3864feb51b69f18f11400144e8cc8
rich <108325206+crich28@users.noreply.github.com>
2022-08-15 05:59:57 -0700

    code cleaning

--- pokeparser.py
+++ pokeparser.py
@@ -1,7 +1,7 @@
 import requests as re
 import csv
 
-class PokeScrapper():
+class PokeScraper():
     
     
     def __init__(self,name):
@@ -9,25 +9,20 @@ class PokeScrapper():
         self.datalist = {}
         self.get_poke_data()
         
-    
     def get_poke_data(self):
         url = 'https://pokeapi.co/api/v2/pokemon/' + str(self.name)
         result = re.get(url)
-
         parse_json = result.json()
 
-
-        abilities = parse_json.get('abilities')
+        abilities = parse_json['abilities']
         name = parse_json['name']
         types = parse_json['types']
         stats = parse_json['stats']
 
-        
-
         self.datalist.update({'name': name})
 
-
         typelist=[]
+
         for i in range(len(types)):
             staginglist = types[i]['type']
             typelist.append(staginglist['name'])
@@ -55,14 +50,13 @@ class PokeScrapper():
         datalist = self.datalist
         
         tags = list(datalist.keys())
-        with open ('pokeparsertest.csv', 'a', newline ='') as new_csv:
+        with open ('pokeparser.csv', 'a', newline ='') as new_csv:
             csv_writer = csv.DictWriter(new_csv,fieldnames=tags)
             csv_writer.writerow(datalist)
 
                 
 
     def poke_print(self,value):
-        
         
         return self.datalist.get(value)
 
@@ -84,27 +78,25 @@ class PokeParser():
 
     def type_checker(self,type):
 
-
-        
-            for pokemon in self.pokedata:
-                if type in pokemon['types']:
-                    self.typelist.append(pokemon)
-            
+        for pokemon in self.pokedata:
+            if type in pokemon['types']:
+                self.typelist.append(pokemon)
+        return self.typelist    
                
     def ability_checker(self,ability):
 
-
-            for pokemon in self.pokedata:
-                if ability in pokemon['abilities']:
-                    self.abilitylist.append(pokemon)
-
+        for pokemon in self.pokedata:
+            if ability in pokemon['abilities']:
+                self.abilitylist.append(pokemon)
+        return self.abilitylist
             
     def stat_checker(self,list):
         
-            for pokemon in self.pokedata:
-                if int(pokemon[list[0]]) >= list[1]:
-                    self.statlist.append(pokemon)
-                     
+        for pokemon in self.pokedata:
+            if int(pokemon[list[0]]) >= list[1]:
+                self.statlist.append(pokemon)
+        return self.statlist
+
     def cross_checker(self,list1,list2):
         crosscheckerlist = []
         for pokemon in list1:
@@ -112,9 +104,10 @@ class PokeParser():
                 if pokemon == pokemon2:
                     crosscheckerlist.append(pokemon)
         self.crosschecker = crosscheckerlist.copy()
+        return self.crosschecker
 # n=1
 # while n < 10:
-#     p1 = PokeScrapper(n)
+#     p1 = PokeScraper(n)
 #     p1.poke_csv()
 #     print(n)
 #     n+=1
@@ -125,8 +118,9 @@ p1 = PokeParser()
 p1.ability_checker('pressure')
 p1.type_checker('fire')
 
-p1.stat_checker(['special-attack',120])
+p1.stat_checker(['attack',80])
 p1.cross_checker(p1.abilitylist,p1.typelist)
-
 p1.cross_checker(p1.crosschecker,p1.statlist)
+p1.stat_checker(['hp',90])
+p1.cross_checker(p1.crosschecker,p1.typelist)
 print(p1.crosschecker)
