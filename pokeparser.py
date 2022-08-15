0026dcb36219a8ee3b0152f42d36dbdee8201119
rich <108325206+crich28@users.noreply.github.com>
2022-08-15 04:35:35 -0700

    changed pokeparser class to pokescapper/add pokeparser class

--- pokeparser.py
+++ pokeparser.py
@@ -55,11 +55,10 @@ class PokeScrapper():
         datalist = self.datalist
         
         tags = list(datalist.keys())
-        with open ('pokeparser.csv', 'a', newline ='') as new_csv:
+        with open ('pokeparsertest.csv', 'a', newline ='') as new_csv:
             csv_writer = csv.DictWriter(new_csv,fieldnames=tags)
-            
-
             csv_writer.writerow(datalist)
+
                 
 
     def poke_print(self,value):
@@ -69,11 +68,62 @@ class PokeScrapper():
 
 
 n=1
-while n < 906:
+while n < 10:
     p1 = PokeScrapper(n)
     p1.poke_csv()
     print(n)
     n+=1
 
 
-# 
+
+class PokeParser():
+
+    def __init__(self):
+        with open('pokeparser.csv', 'r') as csvfile:
+            csvreader = csv.DictReader(csvfile)
+            pokedata = []
+            for pokemon in csvreader:
+                pokedata.append(pokemon)
+        self.pokedata = pokedata
+        self.typelist = []
+        self.abilitylist = []
+        self.statlist = []
+        self.crosschecker =[]
+
+    def type_checker(self,type):
+
+
+        
+            for pokemon in self.pokedata:
+                if type in pokemon['types']:
+                    self.typelist.append(pokemon)
+            
+               
+    def ability_checker(self,ability):
+
+
+            for pokemon in self.pokedata:
+                if ability in pokemon['abilities']:
+                    self.abilitylist.append(pokemon)
+
+            
+    def stat_checker(self,list):
+        
+            for pokemon in self.pokedata:
+                if int(pokemon[list[0]]) >= list[1]:
+                    self.statlist.append(pokemon)
+                     
+    def type_ability_checker(self,list1,list2):
+        for pokemon in list1:
+            for pokemon2 in list2:
+                if pokemon == pokemon2:
+                    self.crosschecker.append(pokemon)
+
+
+p1 = PokeParser()
+p1.ability_checker('blaze')
+p1.type_checker('dragon')
+
+p1.stat_checker(['attack',120])
+p1.type_ability_checker(p1.statlist,p1.typelist)
+print(p1.crosschecker)
